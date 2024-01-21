c76e5f9475e58dc0b319ccb079f40202c3ee043c
mohd-ashif <mohdaszif077@gmail.com>
2024-01-21 13:15:09 +0530

    setup schema

--- backend/index.js
+++ backend/index.js
@@ -5,19 +5,47 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const jwt  = require('jsonwebtoken');
-const multer = ("multer");
+const multer = require('multer');
 const path = require("path");
 const cors = require("cors")
 
 app.use(express.json());
 app.use(cors());
+
 const port = PORT;
 
 //db connection  with mongodb atals 
 mongoose.connect(DB_URL);
 
+//API test
 app.get("/", (req, res)=> {
     res.send("express app is running")
+})
+
+//image  Storage
+const storage = multer.diskStorage({
+    destination: "./upload/images",
+    filename: (req, file, cb) => {
+     return    cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+const upload = multer({
+    storage:storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5, // 5 MB upload limit
+      },
+})
+
+//Creating upload endpoint images
+app.use('/images', express.static('upload/images'));
+
+app.post("/upload", upload.single('product'), (req, res)=> {
+    res.json({
+        succes:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+
 })
 
 app.listen(PORT, (error)=> {
---
+++ backend/model/products.js
@@ -0,0 +1,43 @@
+const mongoose =  require("mongoose")
+
+const ProductSchema = new mongoose.Schema({
+    id:{
+        type:Number,
+        required:true
+
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required: true
+
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    new_price:{
+        type:Number,
+        required:true
+    },
+    old_price:{
+        type:Number,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.mow
+    },
+    available:{
+        type:Boolean,
+        default:true
+    },
+    
+})
+
+const Product = mongoose.model('products',ProductSchema);
+
+module.exports = Product
