e5d120d6040508a01ad579adaa6e597219ad1a4e
mohd-ashif <mohdaszif077@gmail.com>
2024-01-23 15:38:53 +0530

    setup add product

--- admin/src/components/addproduct/AddProduct.jsx
+++ admin/src/components/addproduct/AddProduct.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import upload from "../../assets/upload_area.svg"
 
 const AddProduct = () => {
-  return (
-    <div className='add-product'></div>
-  )
+    return (
+        <div className='add-product'>
+            <div className="addproduct-itemfield">
+                <p>Product Title</p>
+                <input type="text" name='name' placeholder='type here' />
+            </div>
+            <div className="addproduct-itemfield">
+                <p>Product Description</p>
+                <input type="text" name='name' placeholder='type here' />
+            </div>
+            <div className='add-product-price'>
+                <div className="addproduct-itemfield">
+                    <p>Price</p>
+                    <input type="text" name='old_price' placeholder='type here' />
+                </div>
+                <div className="addproduct-itemfield">
+                    <p>Offer Price</p>
+                    <input type="text" name='new_price' placeholder='type here' />
+                </div>
+                
+            </div>
+
+                <div className="addproduct-itemfield">
+                    <p> Category</p>
+                    <select name="category" className='add-product-selector' id="">
+                        <option value="">Women</option>
+                        <option value="">men</option>
+                        <option value="">kid</option>
+                    </select>
+                </div>
+                <div className="addproduct-itemfield">
+                    <label htmlFor="file-input">
+                        <img src={upload} className='upload-icon' alt="Upload" />
+                    </label>
+                    <input type="file" name="image" id='file-input' hidden />
+                </div>
+                <button className='addproduct-btn'>Add</button>
+
+
+        </div>
+    )
 }
 
 export default AddProduct
--- admin/src/pages/admin/Admin.jsx
+++ admin/src/pages/admin/Admin.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import Sidebar from '../../components/sidebar/sidebar'
+import React from 'react';
+import Sidebar from '../../components/sidebar/sidebar';
+import { Routes, Route } from 'react-router-dom';
+import AddProduct from '../../components/addproduct/AddProduct';
+import ListProduct from '../../components/listProduct/ListProduct';
 
 const Admin = () => {
   return (
-    <div className='sidebar'> 
-  <Sidebar />
-  
+    <div className='admin'>
+      <Sidebar />
+      
+        <Routes>
+          <Route path='/addproduct' element={<AddProduct />} />
+          <Route path='/listproduct' element={<ListProduct />} />
+        </Routes>
+ 
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
