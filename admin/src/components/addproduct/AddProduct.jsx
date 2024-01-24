90936323d463af7279a308f91f545ea35a7df517
mohd-ashif <mohdaszif077@gmail.com>
2024-01-24 17:53:57 +0530

    updates

--- admin/src/components/addproduct/AddProduct.jsx
+++ admin/src/components/addproduct/AddProduct.jsx
@@ -1,9 +1,9 @@
-import React , {useState} from 'react'
+import React, { useState } from 'react'
 import upload_image from "../../assets/upload_area.svg";
 import axios from 'axios'
 
 const AddProduct = () => {
-    const [image , setImage] = useState(false);
+    const [image, setImage] = useState(false);
     const [productDetails, setProductDetails] = useState({
         name: "",
         description: "",
@@ -12,43 +12,43 @@ const AddProduct = () => {
         new_price: "",
         old_price: ""
     });
-    
+
 
     const imageHandler = (e) => {
         setImage(e.target.files[0]);
     }
-    
+
 
     const changeHandler = (e) => {
         setProductDetails({ ...productDetails, [e.target.name]: e.target.value });
     };
-    
+
     const addProduct = async () => {
         try {
             const product = { ...productDetails };
-    
+
             // Upload image using FormData 
             const formData = new FormData();
             formData.append('product', image);
-    
+
             const responseUpload = await axios.post('http://localhost:4000/upload', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                 },
             });
-    
+
             const responseData = responseUpload.data;
-    
+
             if (responseData.success) {
                 product.image = responseData.image_url;
-    
+
                 console.log('Image uploaded successfully:', product.image);
-    
+
                 // Add product 
                 const reponseProduct = await axios.post('http://localhost:4000/products', product);
-    
+
                 const responseAddProduct = reponseProduct.data;
-    
+
                 if (responseAddProduct.success) {
                     alert('Product Added');
                     console.log('Product added successfully:', product);
@@ -63,7 +63,7 @@ const AddProduct = () => {
             console.error('Full error object:', error);
         }
     };
-    
+
     return (
         <div className='add-product'>
             <div className="addproduct-itemfield">
@@ -83,25 +83,25 @@ const AddProduct = () => {
                     <p>Offer Price</p>
                     <input value={productDetails.new_price} onChange={changeHandler} type="text" name='new_price' placeholder='type here' />
                 </div>
-                
+
             </div>
 
-                <div className="addproduct-itemfield">
-                    <p> Category</p>
-                    <select value={productDetails.category} onChange={changeHandler} name="category" className='add-product-selector' id="">
-                        <option value="">Women</option>
-                        <option value="">men</option>
-                        <option value="">kid</option>
-                    </select>
+            <div className="addproduct-itemfield">
+                <p> Category</p>
+                <select value={productDetails.category} onChange={changeHandler} name="category" className='add-product-selector' id="">
+                    <option value="">Women</option>
+                    <option value="">men</option>
+                    <option value="">kid</option>
+                </select>
 
-                </div>
-                <div className="addproduct-itemfield">
-                    <label htmlFor="file-input">
-                        <img src={image?URL.createObjectURL(image) :upload_image} className='upload-icon' alt="Upload" />
-                    </label>
-                    <input  onChange={imageHandler} type="file" name="image" id='file-input' hidden />
-                </div>
-                <button onClick={addProduct} className='addproduct-btn'>Add</button>
+            </div>
+            <div className="addproduct-itemfield">
+                <label htmlFor="file-input">
+                    <img src={image ? URL.createObjectURL(image) : upload_image} className='upload-icon' alt="Upload" />
+                </label>
+                <input onChange={imageHandler} type="file" name="image" id='file-input' hidden />
+            </div>
+            <button onClick={addProduct} className='addproduct-btn'>Add</button>
 
 
         </div>
