36958940a9d7a845efe5444cf53cfab610b026b1
mohd-ashif <mohdaszif077@gmail.com>
2024-01-26 17:15:00 +0530

    update login route

--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -1,25 +1,43 @@
 import React, { useState } from 'react';
 import Footer from '../components/Footer/Footer';
+import axios from 'axios'
 
 const LoginSignUp = () => {
   const [state, setState] = useState('Sign Up');
-  const [ formData , setFormData]= useState({
-    username:"",
-    password:"",
-    email:""
-  })
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: ""
+  });
 
-  const changeHandler = (e)=> {
-    e.preventDefault()
-    setFormData({ ...formData,[e.target.name]:e.target.value})
-
-  }
-
-  const login = async () => {
-   console.log("Login function executed", formData)
-
+  const changeHandler = (e) => {
+    e.preventDefault();
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  const login = async () => {
+    console.log("Login function executed", formData);
+    try {
+      const response = await axios.post('http://localhost:4000/products/signup', formData, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const responseData = response.data;
+
+      if (responseData.success) {
+        localStorage.setItem('auth-token', responseData.token);
+        window.location.replace("/");
+      } else {
+        alert(responseData.error);
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+     
+    }
+  };
 
 
   return (
@@ -27,10 +45,10 @@ const LoginSignUp = () => {
       <div className="loginsignup-container">
         <h1>{state}</h1>
         <div className="loginsignup-field">
-  {state === 'Sign Up' ? <input type="text" name="username" value={formData.username} onChange={changeHandler} placeholder='Your name' /> : null}
-  <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder='Email address' />
-  <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder='Password' />
-</div>
+          {state === 'Sign Up' ? <input type="text" name="username" value={formData.username} onChange={changeHandler} placeholder='Your name' /> : null}
+          <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder='Email address' />
+          <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder='Password' />
+        </div>
 
         <button onClick={() => (state === 'Login' ? login() : signUp())}>Continue</button>
         {state === 'Sign Up' ? (
