073c4ba253c2026087a2c011d9ee2c5e9262383d
mohd-ashif <mohdaszif077@gmail.com>
2024-02-01 10:45:32 +0530

    add carousel

--- frontend/src/components/RelatedProducts/RelatedProduct.jsx
+++ frontend/src/components/RelatedProducts/RelatedProduct.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import Item from '../items/Item';
-import data_product from '../../data/data';
+import relatedProduct from '../../data/new_collections';
 
 const RelatedProduct = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+  };
+
   return (
-    <div className='realtedproducts'>
+    <div className='relatedproducts'>
       <h1>Related Products</h1>
       <hr />
-      <div className="realtedproducts-item">
-        {data_product.map((item, i) => {
-          return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-        })}
+      <div className="relatedproducts-item">
+        <Slider {...settings}>
+          {relatedProduct.map((item, i) => (
+            <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+          ))}
+        </Slider>
       </div>
     </div>
-  )
+  );
 }
 
 export default RelatedProduct;
